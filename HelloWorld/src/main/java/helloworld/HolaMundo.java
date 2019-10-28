1ec7b81931a4983be784d6e8ee2840c8c5b613f0
jano3481 <41763601+jano3481@users.noreply.github.com>
2019-10-27 20:34:17 -0500

    Add files via upload
    Primer Repositorio HOLA MUNDO

---
+++ HelloWorld/src/main/java/helloworld/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author jano3
+ */
+public class HolaMundo {
+    public static void main (String arg[]){
+        System.out.println("HOLA MUNDO123");
+    }
+    
+}
